bde3ce2a8501138f5227b8271471d250bbb59c9c
wangqi <wang.qi@qikuyx.com>
2025-01-25 12:40:45 +0800

    Make llama.cpp support deepseek-r1-qwen

--- src/llama.cpp
+++ src/llama.cpp
@@ -6645,6 +6645,11 @@ static void llm_load_vocab(
                 vocab.type_pre = LLAMA_VOCAB_PRE_TYPE_COMMAND_R;
                 vocab.tokenizer_clean_spaces = false;
             } else if (
+                    tokenizer_pre == "qwen2" ||
+                    tokenizer_pre == "deepseek-r1-qwen") {
+                tokenizer_pre = LLAMA_VOCAB_PRE_TYPE_QWEN2;
+                vocab.tokenizer_clean_spaces = false;
+            } else if (
                 tokenizer_pre == "qwen2") {
                 vocab.type_pre = LLAMA_VOCAB_PRE_TYPE_QWEN2;
                 vocab.tokenizer_clean_spaces = false;
