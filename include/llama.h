ad1e46330e399311fd16c8f489e9ff7d8f076584
Pasha Khosravi <khosravipasha@gmail.com>
2026-08-25 12:11:22 -0700

    llama: align PQ2_0 ftype id with published PQ2_0 ggufs (141)
    (cherry picked from commit 5511458a21a624327c1987b8dba2ab75a5bd6a98)

--- include/llama.h
+++ include/llama.h
@@ -156,7 +156,7 @@ extern "C" {
         LLAMA_FTYPE_MOSTLY_NVFP4         = 39, // except 1d tensors
         LLAMA_FTYPE_MOSTLY_Q1_0          = 40, // except 1d tensors
         LLAMA_FTYPE_MOSTLY_Q2_0          = 41, // except 1d tensors
-        LLAMA_FTYPE_MOSTLY_PQ2_0     = 142, // except 1d tensors (Prism group-128 Q2_0)
+        LLAMA_FTYPE_MOSTLY_PQ2_0     = 141, // except 1d tensors (Prism group-128 Q2_0; matches published PQ2_0 ggufs)
 
         LLAMA_FTYPE_GUESSED = 1024, // not specified in the model file
     };
